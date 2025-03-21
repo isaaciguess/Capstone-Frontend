b2deee11b813257f0369e57c95b0fad628f4355e
Isaac Kerwin <isaackerwin123@gmail.com>
2025-03-21 16:01:13 +1100

    Bottom Navigation Bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-import 'package:first_app/registration_form_generator.dart';
 import 'package:flutter/material.dart';
+import 'navigation_bar.dart'; // Import the file with the nav bar code
 
-void main() {
-  runApp(RegistrationFormGenerator());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Regi Master',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const HomeScreen(), // Use the HomeScreen widget defined in bottom_nav_bar.dart
+    );
+  }
 }
-
-
